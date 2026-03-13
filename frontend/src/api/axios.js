a490b2653d1e175e0cd34b929f8ed82c3a4ee323
keshavsharma05 <sharma62005@gmail.com>
2026-03-13 12:41:43 +0530

    connect frontend to railway backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.7:5000/api',
+  baseURL: 'https://smart-college-parkin-system-production.up.railway.app/api',
 });
 
 api.interceptors.request.use(
